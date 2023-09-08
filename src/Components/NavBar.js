3269f0ff5c322f46643daac70d10cdafad80b80a
pmr745 <prathameshrajput7777@gmail.com>
2023-09-08 20:25:55 +0530

    Updated Mobile UI

---
+++ src/Components/NavBar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const NavBar = () => {
+    return (
+        <nav>
+            <span className="logo"><a href="https://pmr745.github.io/PMR745/index.html" target='_blank'>PMR</a></span>
+        </nav>
+    )
+}
